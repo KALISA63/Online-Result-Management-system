92906c7df7dc188da30e1d9819da691c394ff6c7
KALISA63 <kaljama63@gmail.com>
2023-04-05 09:53:02 +0200

    token remover

--- routes/subj.js
+++ routes/subj.js
@@ -8,7 +8,10 @@ const { verifyAdmin } = require('../middlewares/verify');
 
 // REGISTER Subject
 
-router.post('/register',verifyAdmin, async (req, res)=>{
+// router.post('/register',verifyAdmin, async (req, res)=>{
+
+router.post('/register', async (req, res)=>{
+
     const newSubject = new Subject(req.body);
     try {
         const savedSubject  = await newSubject.save();
